56a7e273ab0ede7bcb0da5f10a29098e88e4929a
Eric Oliveira <eric@npx.com.br>
2024-03-11 17:22:20 -0300

    feat: adding db in memory

--- src/factory/user.factory.ts
+++ src/factory/user.factory.ts
@@ -1,14 +1,13 @@
 import Database from '../config/dbConnection';
 import UserRepository from '../repositories/repository';
 import UserService from '../service/user.service';
-
+import loki from 'lokijs';
 export default class UserFactory {
 
   static async createInstance() {
-    const db = new Database({ connection: 'mongodb://localhost:27017/test' })
-    const dbConnection = await db.connect();
+    const db = new loki('system', {})
 
-    const userRepository = new UserRepository({ dbConnection })
+    const userRepository = new UserRepository({ db })
     const userService = new UserService({ userRepository })
 
     // const userRepositoryInstance = new userRepository.default({ db });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import User from "./entity/user.entity"
 import UserFactory from "./factory/user.factory"
 
 
 (async () => {
   const userFactory = await UserFactory.createInstance()
 
-  const result = await userFactory.find({})
-  console.log("🚀 ~ result:", result)
+  await Promise.all(
+    [
+      new User({ first_name: 'John Doe', last_name: 'Doe' }),
+      new User({ first_name: 'Eric Doe', last_name: 'Oliveira' }),
+    ].map((user: User) => userFactory.create(user))
+  )
+
+  const users = await userFactory.find({})
+  console.log("🚀 ~ users:", users)
 
 })()
